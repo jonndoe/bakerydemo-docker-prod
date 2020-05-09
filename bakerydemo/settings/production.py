c55de9292bd21a5ea05fea6751f222d01b6af25f
jonndoe <qb89@yandex.ru>
2020-05-10 00:02:02 +0300

    ch16

--- bakerydemo/settings/production.py
+++ bakerydemo/settings/production.py
@@ -26,7 +26,8 @@ SECURE_SSL_REDIRECT = os.getenv('DJANGO_SECURE_SSL_REDIRECT', 'off') == 'on'
 # Accept all hostnames, since we don't know in advance which hostname will be used for any given Heroku instance.
 # IMPORTANT: Set this to a real hostname when using this in production!
 # See https://docs.djangoproject.com/en/1.10/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(';')
+#ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '*').split(';')
+ALLOWED_HOSTS =['5.63.154.150','localhost','127.0.0.1','[::1]',]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
